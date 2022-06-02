284c62890f8b33f7dbe770cb9a9f53076e9214e5
navi5599 <ivkelava10@gmail.com>
2022-06-02 16:03:33 +0200

    Added list of pokemons to script.js
    Added five pokemons to the array list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,42 @@
-alert('Hellooo World');
+let pokCharizard = {
+  name: 'Charizard',
+  type: ['Fire', 'Flying'],
+  height: 5,
+  abilities: ['Blaze', 'Flamethrower']
+};
 
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokButterfree = {
+  name: 'Butterfree',
+  type: ['Bug', 'Flying'],
+  height: 3,
+  abilities: ['Confuse', 'Sleep Powder']
+};
+
+let pokPikachu = {
+  name: 'Pikachu',
+  type: ['Electric'],
+  height: 1,
+  abilities: ['Quickattack', 'Thunderbolt']
+};
+
+let pokGengar = {
+  name: 'Gengar',
+  type: ['Ghost', 'Poison'],
+  height: 4.5,
+  abilities: ['Cursed Body', 'Nightmare']
+};
+
+let pokMewtwo = {
+  name: 'Mewtwo',
+  type: ['Psychic'],
+  height: 6,
+  abilities: ['Pressure', 'Psychic beam']
+};
+
+let pokemonList = [
+  pokCharizard,
+  pokButterfree,
+  pokPikachu,
+  pokGengar,
+  pokMewtwo
+];
